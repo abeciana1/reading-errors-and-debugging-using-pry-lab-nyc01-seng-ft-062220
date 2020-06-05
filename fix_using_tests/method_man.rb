5bd845c5a265f2739c55e586da182a97c940c7a8
Alex Beciana <alex.beciana@gmail.com>
2020-06-05 17:09:58 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -4,7 +4,9 @@ def start_game(player1, player2)
   "Hello #{player1} & #{playar2}"
 end
 
-binding.pry
+
 def play_game(player2)
+  puts 'Hello'
+  binding.pry
   "#{player1} is better than #{player2}"
 end
